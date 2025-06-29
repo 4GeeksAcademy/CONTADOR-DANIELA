fc1b165c817c0d7a7e67681c04bbd5ed0b12e23b
danipadi13 <159074064+danipadi13@users.noreply.github.com>
2025-06-29 12:42:45 +0000

    First Commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,26 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+export const Home = ({digiSix, digitFive, digitFour, digitThree, digitTwo, digitOne}) => {
+	
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container mt-5">
+			<h1 className="d-flex justify-content-center">
+				Contador de Daniela Padilla THE BEST
+			</h1>
+		
+		<div className="container mt-5">
+			<div className=	"bg-dark d-flex justify-content-p-3 border rounded-pill">
+				<div class Name="clock d-flex align-items-center">
+					<i className="far fa-clock text-white fs-1 px-4 mx-2 ">
+						</i>
+					</div>
+		             <div className="digitSix text-white fs-1 border border-white rounded px-4 mx-2">{digitSix % 10}</div>			
+					 <div className="digitFive text-white fs-1 border border-white rounded px-4 mx-2">{digitFive % 10}</div>
+					 <div className="digitFour text-white fs-1 border border-white rounded px-4 mx-2">{digitFour % 10}</div>
+					 <div className="digitThree text-white fs-1 border border-white rounded px-4 mx-2">{digitThree % 10}</div>
+					 <div className="digitTwo text-white fs-1 border border-white rounded px-4 mx-2">{digitTree % 10}</div>
+					 <div className="digitOne text-white fs-1 border border-white rounded px-4 mx-2">{digitOne % 10}</div>
+		
+				</div></div>	</div>
 	);
-};
-
-export default Home;
+}
